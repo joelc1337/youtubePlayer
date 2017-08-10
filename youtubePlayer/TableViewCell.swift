baf271d3d49c07ebcef8d011aa693aba3c9113ad
Colon <colonjo@c02rr6wug8wp.mtvn.ad.viacom.com>
2017-08-10 16:48:19 -0400

    transport files

---
+++ youtubePlayer/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  youtubePlayer
+//
+//  Created by Colon, Joel on 8/7/17.
+//  Copyright © 2017 Intern. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
